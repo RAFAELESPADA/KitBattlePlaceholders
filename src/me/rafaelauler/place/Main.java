2809cd334a989dfe0c508e0d64475ce2545c07e2
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2025-10-02 08:24:29 -0300

    Update Main.java

--- src/me/rafaelauler/place/Main.java
+++ src/me/rafaelauler/place/Main.java
@@ -1,5 +1,24 @@
 package me.rafaelauler.place;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
+
+public class Main extends JavaPlugin {
+
+
+/*     */   public void onEnable()
+/*     */   {
+	//REGISTER
+	if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null){
+		/* 151 */       Bukkit.getConsoleSender().sendMessage("§e[KITBATTLE] §aPlaceHolderAPI is found!");
+		/* 151 */       Bukkit.getConsoleSender().sendMessage("§e[KITBATTLE] §aHooking into it!");
+	    new PlaceHolderAPIHook(this).register();
+		/* 151 */       Bukkit.getConsoleSender().sendMessage("§e[KITBATTLE] §aPlaceHolderAPI has hooked sucefully!");
+	}
 }
+}
+
+	
+	
